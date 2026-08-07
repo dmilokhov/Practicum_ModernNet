fd8b0449a824626fedf926984257494e59cc4e2f
Dmitry Milokhov <kobi23@yandex.ru>
2026-08-07 16:35:57 +0300

    [Sprint 10] - Added Redis Cache Service

---
+++ EventService/EventService.Application/Interfaces/Cache/ICacheService.cs
@@ -0,0 +1,8 @@
+﻿namespace EventService.Application.Interfaces.Cache;
+
+public interface ICacheService
+{
+    Task<T?> GetAsync<T>(string key);
+    Task<bool> SendAsync<T>(string key, T value, TimeSpan ttl);
+    Task<bool> RemoveAsync(string key);
+}
